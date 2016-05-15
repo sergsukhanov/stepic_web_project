9d40e6445351f5cf21f496c9bc7b5429b7693396
sergey.sukhanov <sergeysukhanov@ya.ru>
2016-05-15 12:41:18 +0400

    Fix: urls

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -3,18 +3,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ask.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'qa.views.home', name='home'),
-    url(r'^login/', 'qa.views.test'),
-    url(r'^signup', 'qa.views.test'),
-    url(r'^question/(?P<id>\d+)', 'qa.views.question', name='question'),
-    url(r'^ask/', 'qa.views.ask', name='ask'),
-    url(r'^popular/', 'qa.views.popular', name='popular'),
-    url(r'^new', 'qa.views.test'),
-    url(r'^answer/$', 'qa.views.answer', name='answer'),
-)
+urlpatterns = patterns('qa.views',                                              
+   url(r'^$', 'test'),                                                              
+   url(r'^login/.*$', 'test', name='login'),                                    
+   url(r'^signup/.*', 'test', name='signup'),                                   
+   url(r'^question/(?P<id>[0-9]+)/$', 'test', name='question'),                 
+   url(r'^ask/.*', 'test', name='ask'),                                         
+   url(r'^popular/.*', 'test', name='popular'),                                 
+   url(r'^new/.*', 'test', name='new'),                                         
+) 
