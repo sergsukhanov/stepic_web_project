91555f95be1b748a12cec151005db0f278f28740
sergey.sukhanov <sergeysukhanov@ya.ru>
2016-05-17 14:45:17 +0400

    Fix: settings

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -18,12 +18,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = [   
-    url(r'^$', 'qa.views.test'),
-    url(r'^login/.*$', 'qa.views.test'),
-    url(r'^signup/.*$', 'qa.views.test'),
-    url(r'^question/\d+/$', 'qa.views.test'),
-    url(r'^ask/.*$', 'qa.views.test'),
-    url(r'^popular/.*$', 'qa.views.test'),
-    url(r'^new/.*$', 'qa.views.test'),
-]
+urlpatterns = patterns('qa.views',                                              
+   url(r'^$', 'test'),                                                              
+   url(r'^login/.*$', 'test', name='login'),                                    
+   url(r'^signup/.*', 'test', name='signup'),                                   
+   url(r'^question/(?P<id>[0-9]+)/$', 'test', name='question'),                 
+   url(r'^ask/.*', 'test', name='ask'),                                         
+   url(r'^popular/.*', 'test', name='popular'),                                 
+   url(r'^new/.*', 'test', name='new'),                                         
+)
--- init.sh
+++ init.sh
@@ -1,11 +1,12 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf 
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-
+sudo /etc/init.d/mysql restart
+mysql -uroot -e "CREATE DATABASE db_stepic_project;"
+mysql -uroot -e "CREATE USER 'django'@'localhost' IDENTIFIED BY 'django';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON db_stepic_project.* TO 'django'@'localhost';"
+mysql -uroot -e "FLUSH PRIVILEGES;"
+/home/box/web/ask/manage.py syncdb
+sudo rm /etc/gunicorn.d/test
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
-
-sudo /etc/init.d/mysql start
-mysql -uroot -e "create database qa"
-#gunicorn -b 0.0.0.0:8080 hello:app
-#./script.sh
