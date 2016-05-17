f816c239d16409307270dc2d502ef232f7ff1476
sergey.sukhanov <sergeysukhanov@ya.ru>
2016-05-17 22:42:16 +0400

    Fix: settings

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -32,8 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
 'qa'
-    #'django.contrib.admin',
-    #'django.contrib.auth',
+    'django.contrib.admin',
+    'django.contrib.auth',
    # 'django.contrib.contenttypes',
     #'django.contrib.sessions',
    # 'django.contrib.messages',
@@ -42,11 +42,11 @@ INSTALLED_APPS = [
 
 MIDDLEWARE_CLASSES = [
    #'django.middleware.security.SecurityMiddleware',
-   # 'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
    # 'django.middleware.common.CommonMiddleware',
    #'django.middleware.csrf.CsrfViewMiddleware',
-   #'django.contrib.auth.middleware.AuthenticationMiddleware',
-   #'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+   'django.contrib.auth.middleware.AuthenticationMiddleware',
+   'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
    # 'django.contrib.messages.middleware.MessageMiddleware',
    #'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
