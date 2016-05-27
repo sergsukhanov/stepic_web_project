d9d8bd4b737a1cd5e32dc73f734e1107145e799c
sergey.sukhanov <sergeysukhanov@ya.ru>
2016-05-27 10:42:39 +0400

    Fix: init 2

--- init.sh
+++ init.sh
@@ -9,4 +9,4 @@ sudo /etc/init.d/gunicorn restart
 mysql -uroot -e "CREATE DATABASE qa"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON * . * TO 'box'@'localhost';"
 mysql -uroot -e "FLUSH PRIVILEGES"
-/home/box/web/ask/manage.py syncdb
+python /home/box/web/ask/manage.py syncdb
