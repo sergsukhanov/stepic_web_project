6739d33c244d4c17ac91386db9008f2e82b35cb0
sergey.sukhanov <sergeysukhanov@ya.ru>
2016-05-27 10:32:02 +0400

    Fix: init

--- init.sh
+++ init.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
+sudo /etc/init.d/mysql restart
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf 
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-
 sudo rm /etc/gunicorn.d/test
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
-
-sudo /etc/init.d/mysql restart
-mysql -uroot -e "CREATE DATABASE qa;"
-mysql -uroot -e "CREATE USER 'box'@'localhost' IDENTIFIED BY 'box;"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON qa.* TO 'box'@'localhost';"
-mysql -uroot -e "FLUSH PRIVILEGES;"
+mysql -uroot -e "CREATE DATABASE qa"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON * . * TO 'box'@'localhost';"
+mysql -uroot -e "FLUSH PRIVILEGES"
 /home/box/web/ask/manage.py syncdb
